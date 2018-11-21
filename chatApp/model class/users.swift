edb7a013b4198929098ff6b8e4d22c1e71f6f7a2
KrishnaGunjal <44728673+KrishnaGunjal@users.noreply.github.com>
2018-11-21 10:06:01 +0530

    Add files via upload
    Added model classes

---
+++ chatApp/model class/users.swift
@@ -0,0 +1,14 @@
+//
+//  users.swift
+//  
+//
+//  Created by Krrish  on 11/11/18.
+//
+
+import Foundation
+import UIKit
+class users: NSObject {
+    var userId : String!
+    var name : String!
+    var email : String!
+}
